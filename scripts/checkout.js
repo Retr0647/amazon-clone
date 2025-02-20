7a9bae290b918fb787809cd3959539c7111baa7d
Jakub Anielak <kuba@eskrytka.net.pl>
2025-02-20 16:49:31 +0100

    made delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import {products} from '../data/products.js'; // named export
-import {cart, removeFromCart} from '../data/cart.js'; // outside folder
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js'; // outside folder
 import formatCurrency from './utils/money.js'; // current folder
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // default export
 import {deliveryOptions} from '../data/deliveryOptions.js';
@@ -95,7 +95,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option"
+            data-product-id="${matchingProduct.id}"
+            data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio" 
                 ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
@@ -126,3 +128,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         container.remove();
     })
 });
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+    })
+})
